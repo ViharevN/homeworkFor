448f24f9345c4ea0e30af9955e28b10dba85c244
ViharevN <swix_off@mail.ru>
2022-10-03 00:40:14 +0300

    задание 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //задание 1
+        int age = 18;
+
+        if (age >= 18) {
+            System.out.println("поздравляем с совершеннолетием");
+        }
+        else {
+            System.out.println("совершеннолетие еще не наступило и нужно еще немного подождать");
+        }
+    }
+}
